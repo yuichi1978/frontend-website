82df57ac8420b2c35a9d2d2b50da68b1885d69ba
YuichiOta <rb19780519@gmail.com>
2023-01-24 15:44:39 +0900

    vite add

---
+++ main.js
@@ -0,0 +1,3 @@
+// import "./style.scss";
+// import "./main";
+// import $ from "jquery"
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // IE11向けの変換を行うプラグインの設定
+  plugins: [
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  server: {
+    port: 8080
+  },
+  base: './'
+})
